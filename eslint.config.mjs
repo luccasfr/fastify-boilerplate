de25327d8032cf29690f22909fa9235022d705c8
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2025-06-24 09:16:29 -0300

    refactor: clean up ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import js from '@eslint/js'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-
   tseslint.configs.recommended,
   eslintPluginUnicorn.configs.recommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', './src/generated/**'],
     rules: {
-      "unicorn/prefer-module": "off", // Disable prefer-module rule
-      "unicorn/filename-case": [
-        "error",
+      'unicorn/prefer-module': 'off', // Disable prefer-module rule
+      'unicorn/filename-case': [
+        'error',
         {
           cases: {
             kebabCase: true,
           },
         },
       ],
-      "unicorn/no-null": "off",
-      "unicorn/prevent-abbreviations": "off",
+      'unicorn/no-null': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
     },
   },
-    {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.node },
-  },
-  {
-    ignores: [
-      "**/dist/**",
-      "**/node_modules/**",
-      "./src/generated/**"
-    ],
-  }
-]);
+])
